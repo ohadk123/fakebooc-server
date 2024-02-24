4c06aa2b07c32dbcf01ade673f697bc5de56dacc
ohadk123 <ohad43@gmail.com>
2024-02-24 15:59:16 +0200

    add /api to apirouter use

--- app.js
+++ app.js
@@ -17,6 +17,6 @@ app.use(express.static("public"));
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.json());
-app.use('/', apiRouter);
+app.use('/api', apiRouter);
 
 app.listen(process.env.PORT);
