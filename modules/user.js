f526f67d6224a9ea8d478c8eb578980d10eba155
ohadk123 <ohad43@gmail.com>
2024-02-24 15:28:01 +0200

    add User model

---
+++ modules/user.js
@@ -0,0 +1,32 @@
+import mongoose from ("mongoose");
+const Schema = mongoose.Schema;
+
+const User = new Schema({
+    username : {
+        type: String,
+        required: true,
+        unique: true
+    },
+    displayName : {
+        type: String,
+        required: true
+    },
+    password : {
+        type: String,
+        required: true
+    },
+    friends : [{
+        type: Schema.Types.ObjectId, 
+        ref: "User"
+    }],
+    friendReq : [{
+        type: Schema.Types.ObjectId, 
+        ref: "User"
+    }],
+    posts : [{
+        type: Schema.Types.ObjectId,
+        ref: "Post"
+    }]
+});
+
+module.exports = mongoose.model('User', User);
