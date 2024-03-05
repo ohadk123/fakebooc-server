948e8f07a0dd0d91e45371e5677c49505b728605
ohadk123 <ohad43@gmail.com>
2024-03-05 23:10:39 +0200

    New models

--- models/comment.js
+++ models/comment.js
@@ -2,6 +2,10 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const Comment = new Schema({
+    post : {
+        type: Schema.Types.ObjectId,
+        ref: "Post"
+    },
     uploader : {
         type: Schema.Types.ObjectId,
         ref: "User"
