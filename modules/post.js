ef768c9fc54154dc4f1bd6dab731879ece4ac689
GalMansuryan <galmanb0108@gmail.com>
2024-03-06 14:27:25 +0200

    u

--- modules/post.js
+++ modules/post.js
@@ -1,4 +1,4 @@
-import mongoose from ("mongoose");
+import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const Post = new Schema({
@@ -8,7 +8,7 @@ const Post = new Schema({
     },
     content : {
         type: String,
-        requied: true
+        required: true
     },
     contentImage : {
         type: String
--- services/post.js
+++ services/post.js
@@ -1,4 +1,5 @@
-import Post from "../modules/post.js";
+
+const Post = require('../modules/post.js')
 
 async function updatePost(jwt, username, pid, content) {
     // Authenticate jwt with username
