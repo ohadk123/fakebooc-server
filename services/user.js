07fb72517c4d242f30917938636870d38032a437
ohadk123 <ohad43@gmail.com>
2024-03-06 11:22:38 +0200

    I heard you like docs, so i added docs in your docs

--- services/user.js
+++ services/user.js
@@ -2,14 +2,19 @@ import User from "../models/user.js";
 import getErrorJson from "./error.js";
 
 /**
- * @returns:
- * On success - The user as it shows in the db
- * if any verification error accrued:
- * status 409
- * errors[0]: username error
- * errors[1]: password error
- * errors[2]: displayName error
- * errors[3]: profileImage error
+ * Check inputs and adds user to the database
+ * @param {String} username - Unique identifier for user
+ * @param {String} displayName - A name that will be displayed on the platform
+ * @param {Base64} profileImage - A profile picture that will be displayed on the platform
+ * @param {String} password - A secret password to authenticate login attempt
+ * @returns :
+ * On success - User information as it shows on db
+ * if any input errors accrued -
+ *      409,
+ *      errors[0]: "Username already in user" - If username already exists in the db
+ *      errors[1]: A concatinated string of password errors
+ *      errors[2]: "Please enter a display name" - If displayName is null
+ *      errors[3]: "Please upload a profile picture" - If profileImage is null
  */
 async function registerUser(username, displayName, profileImage, password) {
     let hasErrors = false;
@@ -70,10 +75,10 @@ async function registerUser(username, displayName, profileImage, password) {
 }
 
 /**
- * Updates a user in db, changes fields displayName and profileImage
- * Only if they are not null
- * @returns:
- * On success - Updated user's information
+ * Get user's information
+ * @param {String} username - User to find
+ * @returns :
+ * On success - username, displayName and profileImage
  * On failure:
  *      404 "User not found" error
  */
@@ -92,6 +97,9 @@ async function getUserInformation(username) {
 /**
  * Updates a user in db, changes fields displayName and profileImage
  * Only if they are not null
+ * @param {String} username - User to update
+ * @param {String} newDisplayName - New display name to update to
+ * @param {Base64} newProfileImage - New profile image to update to
  * @returns:
  * On success - Updated user's information
  * On failure:
