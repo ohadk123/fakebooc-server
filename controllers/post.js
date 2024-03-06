0c0f81dc690af757646fd50843560fb2e776c49c
ohadk123 <ohad43@gmail.com>
2024-03-06 11:11:35 +0200

    Post handling

--- controllers/post.js
+++ controllers/post.js
@@ -1,12 +1,18 @@
 import PostService from "../services/post.js";
-import runController from "./runner.js";
+import {authorizeRequest, runController} from "./runner.js";
 
 // put
 async function updatePost(req, res) {
-    const updatePostData = PostService.updatePost(
-        req.params.pid,
-        req.params
-    );
+    let updatePostData = authorizeRequest(req.user, req.params.username);
+
+    if (!updatePostData)
+        updatePostData = PostService.updatePost(
+            req.params.pid,
+            req.params.content,
+            req.params.contentImage
+        );
+
+    runController(updatePostData, res);
 }
 
 export default {updatePost};
--- services/post.js
+++ services/post.js
@@ -1,7 +1,25 @@
 import Post from "../models/post.js";
+import getErrorJson from "./error.js";
 
-async function updatePost(pid, content, contentImage) {
+async function updatePost(pid, newContent, newContentImage) {
+    const post = await Post.findById(pid);
+    if (!post)
+        return getErrorJson(404, ["Post not found"]);
 
+    let content = post.content;
+    let contentImage = post.contentImage;
+
+    if (newContent)
+        content = newContent;
+    if (newContentImage)
+        contentImage = newContentImage;
+
+    await Post.findByIdAndUpdate(pid, {
+        content: content,
+        contentImage: contentImage
+    });
+
+    return await Post.findById(pid);
 }
 
 export default {updatePost};
