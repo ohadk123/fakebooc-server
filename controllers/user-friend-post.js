d7b181e4cb7d9c0e108d616cb8d6ac19d1988cb8
ohadk123 <ohad43@gmail.com>
2024-03-06 03:48:53 +0200

    User Friend Post handling

--- controllers/user-friend-post.js
+++ controllers/user-friend-post.js
@@ -1,15 +1,23 @@
 import UserFriendPostService from "../services/user-friend-post.js";
+import runController from "./runner.js";
 
 // get
 async function getPostsForFeed(req, res) {
-    // Should get 25 most recenetly uploaded posts
-    // 20 posts by (username)'s friends
-    // 5 posts by not (username)'s friends
+    const getPostsForFeedData = await UserFriendPostService.getPostsForFeed(
+        req.user
+    );
+
+    runController("", "", getPostsForFeedData, res);
 }
 
 // get
 async function getUserPosts(req, res) {
-    // Only available to friends of (posterUsername)
+    const getUserPostsData = await UserFriendPostService.getUserPosts(
+        req.user,
+        req.params.username
+    );
+
+    runController("", "", getUserPostsData, res);
 }
 
 export default {getPostsForFeed, getUserPosts};
--- services/user-friend-post.js
+++ services/user-friend-post.js
@@ -1,14 +1,50 @@
 import User from "../models/user.js";
 import Post from "../models/post.js";
+import getErrorJson from "./error.js";
 
 async function getPostsForFeed(username) {
     // Should get 25 most recenetly uploaded posts
     // 20 posts by (username)'s friends
     // 5 posts by not (username)'s friends
+    if (!(await User.findById(username)))
+        return getErrorJson(404, ["User [" + username + "] not found"]);
+
+    const posts = await Post.find({}).sort("date");
+    const user = await User.findById(username);
+    const feed = [];
+
+    let friendsPostsLeft = 20;
+    let othersPostsLeft = 5;
+
+    while(posts.length > 0) {
+        let post = posts.pop();
+        let uploader = await User.findById(post.uploader);
+
+        if (username == uploader)
+            continue;
+
+        if (user.friends.includes(uploader._id)) {
+            if (friendsPostsLeft > 0) {
+                feed.unshift(post);
+                friendsPostsLeft--;
+            }
+        } else if (othersPostsLeft > 0) {
+            feed.unshift(post);
+            othersPostsLeft--;
+        } else break;
+    }
+
+    return {posts: feed};
 }
 
-async function getUserPosts(connectedUsername, posterUsername) {
+async function getUserPosts(connectedUsername, uploaderUsername) {
     // Only available to friends of (posterUsername)
+    const areFriends = (await User.findById(connectedUsername)).friends.includes(uploaderUsername);
+    if (!areFriends && connectedUsername !== uploaderUsername)
+        return getErrorJson(403, ["Forbidden access to [" + uploaderUsername + "]'s posts"]);
+
+    const posts = Post.find({uploader: uploaderUsername});
+    return posts;
 }
 
 export default {getPostsForFeed, getUserPosts};
