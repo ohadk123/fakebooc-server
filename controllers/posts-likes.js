b6385f622aa14cef7cf4cf93706f6ed6dbdd64a7
ohadk123 <ohad43@gmail.com>
2024-03-06 14:45:51 +0200

    missing awaits

--- controllers/posts-likes.js
+++ controllers/posts-likes.js
@@ -2,7 +2,7 @@ import PostLikesService from "../services/posts-likes.js";
 import Runner from "./runner.js";
 
 async function getLikes(req, res) {
-    const getLikesData = PostLikesService.getLikes(
+    const getLikesData = await PostLikesService.getLikes(
         req.params.pid
     );
 
@@ -10,7 +10,7 @@ async function getLikes(req, res) {
 }
 
 async function addLike(req, res) {
-    const addLikeData = PostLikesService.addLike(
+    const addLikeData = await PostLikesService.addLike(
         req.user,
         req.params.pid
     );
@@ -19,7 +19,7 @@ async function addLike(req, res) {
 }
 
 async function removeLike(req, res) {
-    const removeLikeData = PostLikesService.removeLike(
+    const removeLikeData = await PostLikesService.removeLike(
         req.user,
         req.params.pid
     );
