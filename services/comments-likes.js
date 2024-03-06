b787e32ff67efe730b0b0fc35a96910271be85fc
ohadk123 <ohad43@gmail.com>
2024-03-06 13:47:28 +0200

    likes docs

--- services/comments-likes.js
+++ services/comments-likes.js
@@ -1,6 +1,14 @@
 import Comment from "../models/comment.js";
 import getErrorJson from "./error.js";
 
+/**
+ * Get a list of users who liked a comment
+ * @param {Schema.Types.ObjectId} cid - Comment id to get likes of
+ * @returns :
+ * On success - A list of users who liked cid
+ * On failure -
+ *      404, "Comment not found" - If cid doesn't exist in db
+ */
 async function getLikes(cid) {
     const comment = await Comment.findById(cid);
     if (!comment)
@@ -9,6 +17,16 @@ async function getLikes(cid) {
     return comment.likes
 }
 
+/**
+ * Adds username to list of likes in comment
+ * @param {String} username - User to like comment
+ * @param {Schema.Types.ObjectId} cid - comment to like
+ * @returns :
+ * On success - The updated comment
+ * On failure -
+ *      404, "Comment not found" - If cid doesn't exist in db
+ *      400, "Comment already liked by [username]" - If comment is already liked by username
+ */
 async function addLike(username, cid) {
     const comment = await Comment.findById(cid);
     if (!comment)
@@ -24,6 +42,16 @@ async function addLike(username, cid) {
     });
 }
 
+/**
+ * removes username from list of likes in comment
+ * @param {String} username - User to remove like comment
+ * @param {Schema.Types.ObjectId} cid - comment to remove like from
+ * @returns :
+ * On success - The updated comment
+ * On failure -
+ *      404, "Comment not found" - If cid doesn't exist in db
+ *      400, "Comment is not liked by [username]" - If comment is not liked by username
+ */
 async function removeLike(username, cid) {
     const comment = await Comment.findById(cid);
     if (!comment)
--- services/posts-likes.js
+++ services/posts-likes.js
@@ -1,6 +1,14 @@
 import Post from "../models/post.js";
 import getErrorJson from "./error.js";
 
+/**
+ * Get a list of users who liked a post
+ * @param {Schema.Types.ObjectId} pid - Post id to get likes of
+ * @returns :
+ * On success - A list of users who liked pid
+ * On failure -
+ *      404, "Post not found" - If pid doesn't exist in db
+ */
 async function getLikes(pid) {
     const post = await Post.findById(pid);
     if (!post)
@@ -9,6 +17,16 @@ async function getLikes(pid) {
     return post.likes
 }
 
+/**
+ * Adds username to list of likes in post
+ * @param {String} username - User to like post
+ * @param {Schema.Types.ObjectId} pid - post to like
+ * @returns :
+ * On success - The updated post
+ * On failure -
+ *      404, "Post not found" - If pid doesn't exist in db
+ *      400, "Post already liked by [username]" - If post is already liked by username
+ */
 async function addLike(username, pid) {
     const post = await Post.findById(pid);
     if (!post)
@@ -24,6 +42,16 @@ async function addLike(username, pid) {
     });
 }
 
+/**
+ * removes username from list of likes in post
+ * @param {String} username - User to remove like post
+ * @param {Schema.Types.ObjectId} pid - post to remove like from
+ * @returns :
+ * On success - The updated post
+ * On failure -
+ *      404, "Post not found" - If pid doesn't exist in db
+ *      400, "Post is not liked by [username]" - If post is not liked by username
+ */
 async function removeLike(username, pid) {
     const post = await Post.findById(pid);
     if (!post)
