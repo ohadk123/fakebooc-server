2d7b6edfedf99e565c50b24c1b3d93131313b566
GalMansuryan <galmanb0108@gmail.com>
2024-03-20 13:17:01 +0200

    build connected

--- app.js
+++ app.js
@@ -5,13 +5,27 @@ import mongoose from "mongoose";
 import customEnv from "custom-env";
 import apiRouter from "./routes/api.js";
 import audit from "express-requests-logger";
+import path from "path";
+import { fileURLToPath } from "url";
+
 customEnv.env(process.env.NODE_ENV, "./config");
 console.log(process.env.CONNECTION_STRING);
 console.log(process.env.PORT);
 
 mongoose.connect(process.env.CONNECTION_STRING, {});
+const __filename = fileURLToPath(import.meta.url);
+console.log("file name:", __filename);
+const __dirname = path.dirname(__filename);
+console.log("dir name:", __dirname);
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, "build")));
+// Handles any requests that don't match the ones above
+app.get("/", (req, res) => {
+  console.log("boubou");
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.use(express.static("public"));
 app.use(cors());
